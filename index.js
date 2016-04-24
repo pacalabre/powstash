da0af79d063e91338b4d56e8ac63f2b617129ca7
Paul <pacalabre@gmail.com>
2016-04-23 23:01:44 -0700

    Added styles, and routes

---
+++ index.js
@@ -0,0 +1,57 @@
+var express= require('express'),
+app = express();
+var bodyParser = require('body-parser')
+var request = require('request');
+app.use(express.static(__dirname + '/views'));
+
+app.set('view engine', 'ejs');
+
+app.use( bodyParser.urlencoded({extended: false }) );
+
+quotes = [
+"If you French Fry when you supposed to Pizza, you're gonna have a bad time -South Park",
+'Just ski down there and jump off something for crying out loud! - Shane McConkey',
+'We’re skiers from day 1. We’ll be skiers till the day we die - Tanner Hall'
+]
+
+app.get('/', function(req, res) {
+  // use sendFile to render the index page
+  res.render('index.ejs');
+});
+
+app.get('/about', function(req, res) {
+  // use sendFile to render the index page
+  res.render('about.ejs');
+});
+
+app.get('/weather', function(req, res) {
+var query = req.query.q;
+var url = 'http://api.openweathermap.org/data/2.5/weather?q=';
+var key = '&APPID=8057a36377e08e0ab0adb6578fabe5f2';
+console.log(query);
+
+  // res.render('weather');
+
+  request(url+query+key, function(err, response, body) {
+    var data = JSON.parse(body);
+    console.log(data);
+    if(!err && response.statusCode === 200 && data) {
+      res.render('weather',{conditions:data,q:query})
+    } else {
+      res.render('error');
+    }
+  })
+})
+
+app.get('/myresorts', function(req, res){
+  // use sendFile to render the index page
+  res.render('myresorts.ejs');
+});
+
+
+app.listen(3000, function(){
+});
+
+
+//JSON.stringify(variable)
+// [object:]
---
+++ wireframes + demographics/UserStories.md
@@ -0,0 +1,41 @@
+Demographic:
+
+- Active passionate skier / snowboarder
+- Primary target audience early 20's - mid 30's
+- tech savvy
+- lives in areas with a large selection of ski mtns
+
+User story:
+
+Rider is good to the mtn and wants to know who has the best conditions
+
+  Situation 1:
+
+    It has been a rough snow year and the rider wants to know which mtn may have biggest base of snow. Rider checks the app to compare snow current snow depth.
+
+  Situation 2:
+
+    It is a powder day and the rider wants to know which of their local mountain has the most gnar gnar. Rider checks the app to compare snowfall in the last 48 hours, and the last 24 hrs.
+
+  Situation 3:
+
+    Rider is choosing which mtn to spend a weekend getaway at with some friends. Rider looks at the app to see whom has the biggest base, and the most recent snowfall -  but also what the projected snowfall is for the next 24 - 48 hrs.
+
+
+*** Build on feature: tell how long the current drive is to the resort, and how bad traffic is.
+
+- Home / Search Page
+- Results page
+- Saved "My Resorts page" that stores your saved resorts
+
+
+API's:
+http://feeds.snocountry.net/
+Open Weather
+Google Map
+wunderground api
+
+
+-autofill resort
+-merge saved page and home page if logged in
+-avalanche danger
